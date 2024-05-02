a7760916171f9556d515cefe3770f9ee389980a0
Florian Pfaff <pfaff@ias.uni-stuttgart.de>
2024-05-02 21:02:08 +0100

    More cleanup

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import os
 import hydra
 
-# from score_sde.utils.cfg import *
-
-
 @hydra.main(config_path="config", config_name="main", version_base='1.1')
 def main(cfg):
     os.environ["GEOMSTATS_BACKEND"] = "jax"
     os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
     os.environ["WANDB_START_METHOD"] = "thread"
-    # os.environ["JAX_ENABLE_X64"] = "True"
 
     from run import run
 
