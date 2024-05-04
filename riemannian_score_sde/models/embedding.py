705156bd1c58b019f3cdecd9b0ee99accefccdb7
Florian Pfaff <pfaff@ias.uni-stuttgart.de>
2024-05-04 18:49:03 +0000

    Using geomstats backend for embedding.py

--- riemannian_score_sde/models/embedding.py
+++ riemannian_score_sde/models/embedding.py
@@ -2,9 +2,10 @@ import abc
 from dataclasses import dataclass
 
 import haiku as hk
-import jax.numpy as jnp
 
 from score_sde.models import MLP
+from geomstats.backend import ones, array, cos, sin, expand_dims, concatenate, arange
+from math import pi
 
 
 class Embedding(hk.Module, abc.ABC):
@@ -22,20 +23,20 @@ class LaplacianEigenfunctionEmbedding(Embedding):
     def __init__(self, manifold, n_manifold, n_time, max_t):
         super().__init__(manifold)
         self.n_time = n_time
-        self.frequencies = 2 * jnp.pi * jnp.arange(n_time) / max_t
+        self.frequencies = 2 * pi * arange(n_time) / max_t
         self.n_manifold = n_manifold
 
     def __call__(self, x, t):
-        t = jnp.array(t)
+        t = array(t)
         if len(t.shape) == 0:
-            t = t * jnp.ones(x.shape[:-1])
+            t = t * ones(x.shape[:-1])
 
         if len(t.shape) == len(x.shape) - 1:
-            t = jnp.expand_dims(t, axis=-1)
+            t = expand_dims(t, axis=-1)
 
         x = self.manifold.laplacian_eigenfunctions(x, self.n_time)
-        t = jnp.concatenate(
-            (jnp.cos(self.frequencies * t), jnp.sin(self.frequencies * t)), axis=-1
+        t = concatenate(
+            (cos(self.frequencies * t), sin(self.frequencies * t)), axis=-1
         )
 
         return x, t
@@ -48,11 +49,11 @@ class ConcatEigenfunctionEmbed(hk.Module):
         self._layer = MLP(hidden_shapes=hidden_shapes, output_shape=output_shape, act=act)
 
     def __call__(self, x, t):
-        t = jnp.array(t)
+        t = array(t)
         if len(t.shape) == 0:
-            t = t * jnp.ones(x.shape[:-1])
+            t = t * ones(x.shape[:-1])
 
         if len(t.shape) == len(x.shape) - 1:
-            t = jnp.expand_dims(t, axis=-1)
+            t = expand_dims(t, axis=-1)
 
-        return self._layer(jnp.concatenate([x, t], axis=-1))
+        return self._layer(concatenate([x, t], axis=-1))
